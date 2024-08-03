551d0f94bf813b79a859a5d6a2c86a44be8e949f
Eduard Lupu <lupu.edward2@gmail.com>
2024-08-03 19:19:32 +0300

    Added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,15 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Studio Pill",
-  description: "Work in progress"
+  description: "Work in progress",
+  icons: {
+    icon: [
+      { rel: "icon", url: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
+      { rel: "icon", url: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
+    ],
+    shortcut: { url: "/favicon.ico" }
+  },
+  manifest: "/site.webmanifest"
 };
 
 export default function RootLayout({
