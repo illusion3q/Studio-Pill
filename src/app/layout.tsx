4054c8bdd68371e1afbfd4cdf769edd94bfa0098
lupumihail <lupu.miihail@gmail.com>
2024-08-04 14:12:41 +0300

    Removed unused files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,9 @@ export const metadata: Metadata = {
     icon: [
       { rel: "icon", url: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
       { rel: "icon", url: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
-      { rel: "apple-touch-icon", url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }
+      { rel: "apple-touch-icon", url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+      { rel: "favicon-16x16", url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" }
+      { rel: "favicon-32x32", url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" }
     ],
     shortcut: { url: "/favicon.ico" }
   },
